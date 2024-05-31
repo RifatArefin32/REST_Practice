e0b0bdbbb1f6da713fd7714490ddeae080b14722
RifatArefin32 <arefinrifat786@gmail.com>
2024-05-31 17:40:04 +0600

    31052024 1740

--- views/task/index.php
+++ views/task/index.php
@@ -21,9 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Task', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-    <?php 
-     echo $this->render('_search', ['model' => $searchModel]); 
-    ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
